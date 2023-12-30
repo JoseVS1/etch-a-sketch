b882ed99a448c81a8f37ade1b73679e6cbe26ce4
JoseVS1 <105169283+JoseVS1@users.noreply.github.com>
2023-12-30 11:00:53 -0600

    Complete functionality and random square colors

---
+++ main.js
@@ -0,0 +1,37 @@
+const container = document.querySelector(".container");
+const gridBtn = document.querySelector(".grid-btn");
+
+const changeSquareColor = (e) => {
+    const currSquare = e.target;
+    currSquare.style.backgroundColor = `rgb(${Math.random() * 255} ${Math.random() * 255} ${Math.random() * 255})`;
+}
+
+const generateGrid = (size = 16) => {
+    for (let i = 0; i < size * size; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        square.style.width = `${(960 / size) - 2}px`;
+        square.style.height = `${(960 / size) - 2}px`;
+        square.addEventListener("dragenter", (e) => changeSquareColor(e));
+        container.appendChild(square);
+    }
+}
+
+generateGrid();
+
+const changeGridSize = () => {
+    let newSize;
+    do {
+        newSize = Number(prompt("Enter the number of squares per side for the new grid:"));
+    } while (newSize > 100);
+    console.log(newSize)
+
+    while (container.firstChild) {
+        container.firstChild.remove();
+    }
+
+    generateGrid(newSize);
+}
+
+
+gridBtn.addEventListener("click", changeGridSize);
